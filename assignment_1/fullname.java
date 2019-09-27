ed32bd914f713a207b1f5e84933c62c94b0e0c07
Akosua Kernizan <akosua@Akosuas-Air.home>
2019-09-26 21:43:57 -0400

    updated variables

--- assignment_1/fullname.java
+++ assignment_1/fullname.java
@@ -2,13 +2,13 @@ public class fullname {
   public static void main(String[] args){
     String firstName = "Akosua";
     String lastName = "Kernizan";
-    String myFullyName = firstName + ' ' + lastName;
+    String myFullName = firstName + ' ' + lastName;
 
-    int birthDate = 4;
+    int birthDay = 4;
     int birthMonth = 6;
     int birthYear = 1989;
 
-    String myBirthday = Integer.toString(birthMonth) + "-" + Integer.toString(birthDate)+ "-" + Integer.toString(birthYear);
+    String myBirthday = Integer.toString(birthMonth) + "-" + Integer.toString(birthDay)+ "-" + Integer.toString(birthYear);
 
     System.out.println("    *********        ******        ******   ");
     System.out.println("   ****   ****       ******      ******     ");
@@ -20,7 +20,7 @@ public class fullname {
     System.out.println(" ****        ****    ******      ******     ");
     System.out.println(" ****        ****    ******        *******  ");
 
-    System.out.println("My name is " + myFullyName);
+    System.out.println("My name is " + myFullName);
     System.out.println("My birthday is on " + myBirthday);
   }
 }
